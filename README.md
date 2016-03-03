7b4bd9e37b8d22baa47668e79636ee951894b73e
t1s1 <tedesi@gmail.com>
2016-03-03 18:52:11 +0000

    minor changes so it will render

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Vision Rocket
+
+Responsive with Bootstrap
+
+*Run npm start to compile*
--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,4 @@
 var React = require("react");
 var ReactDom = require("react-dom");
 
+ReactDom.render(<h4>Nothing Here Yet</h4>, document.getElementById("main"));
