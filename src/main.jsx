956201918a0b8b09af1c03d2115752a0ddd4ea43
t1s1 <tedesi@gmail.com>
2016-03-21 18:30:43 +0000

    minor changes

--- src/main.jsx
+++ src/main.jsx
@@ -2,4 +2,4 @@ var React = require("react");
 var ReactDom = require("react-dom");
 var Shell = require("./components/Shell.jsx");
 
-ReactDom.render(<Shell/>, document.getElementById("main"));
+ReactDom.render(<Shell />, document.getElementById("main"));
